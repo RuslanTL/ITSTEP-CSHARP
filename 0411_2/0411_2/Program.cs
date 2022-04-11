7521673fd148142dae7431f644f7d9b4f1233ee4
plaz <53999108+RuslanTL@users.noreply.github.com>
2022-04-11 17:20:40 +0600

    1

---
+++ 0411_2/0411_2/Program.cs
@@ -0,0 +1,112 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//    Problem 01
+// Напишише программу, принимающую 4 параметра командной строки, первые два из которых которые должны быть целыми числами, а последние -
+// вещественными.Уведомляйте пользователей о возможных ошибках, как то:
+// - число параметров не равно 4;
+// - параметры не удовлетворяют требованиям и т.п.
+
+global using static System.Console;
+
+
+
+//Problem 04
+//Напишите программу copyAndCount.exe, которая при запуске с двумя параметрами:
+//copyAndCount.exe fileName1 fileName2
+//производит построчное копирование существующего текстового файла fileName1 в новый fileName2 с выводом на консоль количества имеющихся строк и символов в fileName1.
+
+
+class Program
+{
+  static void CopyAndCount(string f1)
+  {
+    using (var file = new StreamReader(f1))
+    {
+      using (var newFile = new StreamWriter("file2.txt"))
+      {
+        string line;
+        int lineCount = 0;
+        int charCount = 0;
+        while ((line = file.ReadLine()) != null)
+        {
+          newFile.WriteLine(line);
+          WriteLine($"line written into new file: ${line}");
+          lineCount++;
+          foreach (char c in line)
+          {
+            charCount++;
+          }
+
+
+        }
+        WriteLine($"Amount of lines: {lineCount}");
+        WriteLine($"Amount of characters: {charCount}");
+      }
+
+
+    }
+  }
+  static void CheckNumbers(string[] args)
+  {
+
+    //    Problem 01
+    //Напишише программу, принимающую 4 параметра командной строки,
+    //первые два из которых которые должны быть целыми числами, а последние -вещественными.Уведомляйте пользователей о возможных ошибках, как то:
+    //    -число параметров не равно 4;
+    //    -параметры не удовлетворяют требованиям и т.п.
+
+    if (args.Length != 4)
+    {
+      WriteLine("the amount of parameters must be 4!");
+      return;
+    }
+
+    bool result;
+    int n1, n2; double d1, d2;
+    result = int.TryParse(args[0], out n1);
+    if (!result)
+    {
+      WriteLine("the first parameter is not an integer!");
+      return;
+    }
+    result = int.TryParse(args[1], out n2);
+    if (!result)
+    {
+      WriteLine("the second parameter is not an integer!");
+      return;
+    }
+    result = double.TryParse(args[2], out d1);
+    if (!result)
+    {
+      WriteLine("the third parameter is not a double!");
+      return;
+    }
+    result = double.TryParse(args[3], out d2);
+    if (!result)
+    {
+      WriteLine("the fourth parameter is not a double");
+      return;
+    }
+    WriteLine("all parameters are correct");
+    return;
+
+  }
+  static void Temp()
+  {
+    int size = 10;
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+      WriteLine($"Enter number {i}: ");
+      string s = ReadLine();
+      int.TryParse(s, out var n);
+      arr[i] = n;
+    }
+  }
+  static void Main(string[] args)
+  {
+    //CopyAndCount("file1.txt");
+    CheckNumbers(args);
+  }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,168 @@
+﻿global using static System.Console;
+
+namespace DemoNS
+{
+
+  class Program
+  {
+    const string _fileName = "demoFile.txt";
+
+    static void CommandLine(string[] args)
+    {
+      //string param1 = args[0];
+      //string param2 = args[2];
+
+      for (int i = 0; i < args.Length; i++)
+      {
+        WriteLine(args[i]);
+      }
+
+      //or:
+      foreach (var item in args)
+      {
+        WriteLine(item);
+      }
+    }
+
+
+
+    static void ReadTwoNumbersFromString()
+    {
+      while (true)
+      {
+        WriteLine("Введите 2 целых числа в одной строке:");
+        string input = Console.ReadLine();
+        int n0, n1;
+
+        //simple, if one space:
+        //string[] words = input.Split(' ');
+
+        //simple, if many spaces:
+        //string[] words = input.Split(' ',  StringSplitOptions.RemoveEmptyEntries);
+
+        //the best!
+        string[] words = input.Split(new char[] { ' ', '\t', ';', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (words.Length != 2)
+        {
+          WriteLine("Не введены 2 числа");
+          continue;
+        }
+        bool result0 = int.TryParse(words[0], out n0);
+        bool result1 = int.TryParse(words[1], out n1);
+        if (result0 && result1)
+        {
+          WriteLine($"n0 = {n0}; n1 = {n1}");
+        }
+        else
+        {
+          WriteLine("Ошибка!");
+        }
+      }
+    }
+
+
+
+
+    static void WriteNumbersToTextFile()
+    {
+      using (var file = new StreamWriter(_fileName))
+      {
+        for (int i = 0; i < 10; i++)
+        {
+          double x = i * i + 0.1;
+          file.WriteLine($"{i}; {x}");
+        }
+      }
+      WriteLine("File has been written");
+
+      //or with using declarations
+      using var file2 = new StreamWriter(_fileName);
+      for (int i = 0; i < 10; i++)
+      {
+        double x = i * i + 0.1;
+        file2.WriteLine($"{i}; {x}");
+      }
+      WriteLine("File has been written");
+    }
+
+
+
+    static void ReadTextFile()
+    {
+      using (var file = new StreamReader(_fileName))
+      {
+        string line;
+        while ((line = file.ReadLine()) != null)
+        {
+          WriteLine(line);
+        }
+      }
+      WriteLine("File has been readed");
+    }
+
+
+
+    static void ReadNumbersFromTextFile()
+    {
+      using (var file = new StreamReader(_fileName))
+      {
+        int n1; double n2;
+        string line;
+        while ((line = file.ReadLine()) != null)
+        {
+          string[] tokens = line.Split(';', StringSplitOptions.RemoveEmptyEntries);
+          bool result0 = int.TryParse(tokens[0], out n1);
+          bool result1 = double.TryParse(tokens[1], out n2);
+          WriteLine($"{n1,5}{n2,6}{n1 * n2,10:N3}");
+        }
+      }
+      WriteLine("File has been readed");
+    }
+
+
+
+    static void ReadAndWrite()
+    {
+      string fileInName = @"C:\_0\students_10_000_000.txt";
+      string fileOutName = @"C:\_0\_temp.txt";
+
+      using var inFile = new StreamReader(fileInName);
+      using var outFile = new StreamWriter(fileOutName);
+      string line;
+      int counter = 0;
+      while ((line = inFile.ReadLine()) != null)
+      {
+        outFile.WriteLine($"{counter}: {line}");
+        counter++;
+        if (counter == 10)
+          break;
+      }
+    }
+
+
+
+    static void Main(string[] args)
+    {
+      try
+      {
+        //CommandLine(args);
+
+        ReadTwoNumbersFromString();
+
+        //WriteNumbersToTextFile();
+        //ReadTextFile();
+        //ReadNumbersFromTextFile();
+
+        //ReadAndWrite();
+
+      }
+      catch (Exception ex)
+      {
+        WriteLine(ex.Message);
+      }
+      WriteLine("\nEnd . . .");
+      ReadLine();
+    }
+  }
+}

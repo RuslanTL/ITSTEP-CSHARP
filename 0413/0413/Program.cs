9a5b6f983e2afe6dccaf97b121a571f72819f181
plaz <53999108+RuslanTL@users.noreply.github.com>
2022-04-18 17:48:35 +0600

    0418_2

--- 0413/0413/Program.cs
+++ 0413/0413/Program.cs
@@ -89,34 +89,33 @@ static void SortInside(long[] arr, int iMin, int iMax).
 
   Сортировка по столбцам (сортировка всех столбцов по отдельности)
   static void SortCols(int[,] mat) {*/
-  static void SortRow(int[,] mat, int row) {
-    int rowLength = mat.GetLength(1);
+  //static void SortRow(int[,] mat, int row) {
+  //    int rowLength = mat.GetLength(1);
+  //    for (int i = 0; i < rowLength - 1; i++) {
+  //        for (int j = 0; j < rowLength - i - 1; j++) {
+  //            if (mat[row, j] > mat[row, j + 1]) {
+  //                int temp = mat[row, j];
+  //                mat[row, j] = mat[row, j + 1];
+  //                mat[row, j + 1] = temp;
+  //            }
+  //        }
 
-    for (int i = 0; i < rowLength - 1; i++) {
-      for (int j = 0; j < rowLength - i - 1; j++) {
-        if (mat[row, j] > mat[row, j + 1]) {
-          int temp = mat[row, j];
-          mat[row, j] = mat[row, j + 1];
-          mat[row, j + 1] = temp;
-        }
-      }
+  //    }
+  //}
+  //static void SortCol(int[,] mat, int col) {
+  //    int colLength = mat.GetLength(0);
+  //    for (int i = 0; i < colLength - 1; i++) {
+  //        for (int j = 0; j < colLength - i - 1; j++) {
+  //            if (mat[j, col] > mat[j + 1, col]) {
 
-    }
-  }
-  static void SortCol(int[,] mat, int col) {
-    int colLength = mat.GetLength(0);
-    for (int i = 0; i < colLength - 1; i++) {
-      for (int j = 0; j < colLength - i - 1; j++) {
-        if (mat[j, col] > mat[j + 1, col]) {
+  //                int temp = mat[j, col];
+  //                mat[j, col] = mat[j + 1, col];
+  //                mat[j + 1, col] = temp;
+  //            }
 
-          int temp = mat[j, col];
-          mat[j, col] = mat[j + 1, col];
-          mat[j + 1, col] = temp;
-        }
-
-      }
-    }
-  }
+  //        }
+  //    }
+  //}
   static void SortRows(int[,] mat) {
     int rowLength = mat.GetLength(1);
     int colLength = mat.GetLength(0);
@@ -150,23 +149,116 @@ static void SortInside(long[] arr, int iMin, int iMax).
         }
       }
     }
-      
+
   }
 
-    /*
-    Problem 03
-    Напишите метод
-    public static int MaxIdenticalNumbersCount(int[] arr),
-    возврашающий максимальное количество одинаковых чисел в массиве.8*/
-  public static int MaxIdenticalNumbersCount(int[] arr) {
-    Tuple<int, int>[] cnt = { };
-    for(int i = 0; i < arr.Length; i++) {
-      for(int j=0; j < cnt.Length; j++) {
+  static void SortRow(int[,] mat, int row) {
+    int rowLength = mat.GetLength(1);
+    int[] rowCopy = new int[rowLength];
 
+
+    for (int i = 0; i < rowLength; i++) {
+      rowCopy[i] = mat[row, i];
+
+    }
+    Write($"\n selected row: \n");
+    for (int i = 0; i < rowCopy.Length; i++) {
+      Write($"{rowCopy[i],4}");
+    }
+    Array.Sort(rowCopy);
+    for (int i = 0; i < rowLength; i++) {
+      mat[row, i] = rowCopy[i];
+
+    }
+
+    Write("\n\n");
+  }
+
+  static void SortCol(int[,] mat, int col) {
+    int colLength = mat.GetLength(1);
+    int[] colCopy = new int[colLength];
+
+
+    for (int i = 0; i < colLength; i++) {
+      colCopy[i] = mat[i, col];
+
+    }
+    Write($"\n selected col: \n");
+    for (int i = 0; i < colCopy.Length; i++) {
+      Write($"{colCopy[i],4}");
+    }
+    Array.Sort(colCopy);
+    for (int i = 0; i < colLength; i++) {
+      mat[i, col] = colCopy[i];
+
+    }
+
+    Write("\n\n");
+  }
+  /*
+  Problem 03
+  Напишите метод
+  public static int MaxIdenticalNumbersCount(int[] arr),
+  возврашающий максимальное количество одинаковых чисел в массиве.8*/
+  public static int MaxIdenticalNumbersCount(int[] arr) {
+    var counts = new Dictionary<int, int>();
+    Array.Sort(arr);
+    for (int i = 0; i < arr.Length; i++) {
+      Write($"{arr[i],3}");
+    }
+    WriteLine(" ");
+    int num = 0;
+    int cnt = 1;
+    for (int i = 0; i < arr.Length - 1; i++) {
+      if (arr[i] == arr[i + 1]) {
+        cnt++;
+        num = arr[i];
+      } else {
+        counts.Add(arr[i], cnt);
+        cnt = 1;
+        num = arr[i + 1];
       }
     }
-    return 0;
+    int max = 0;
+    int maxKey = 0;
+    for (int i = 0; i < counts.Count; i++) {
+      if (counts.ElementAt(i).Value > max) {
+        max = counts.ElementAt(i).Value;
+        maxKey = counts.ElementAt(i).Key;
+      }
+      Write($"{counts.ElementAt(i),3}");
+    }
+    WriteLine($"\nmax amount of occurances: {maxKey} found {max} times in array");
+    return num;
   }
+
+  /*Problem OrderedMatrix
+Матрица сортируется в порядке возрастания сначала по строкам, затем - по столбцам.
+Оказывается (попробуйте доказать самостоятельно!), что после сортировки по столбцам, строки остаются по-прежнему в отсортированном состоянии!
+
+Напишите программу для проверки данной теоремы.
+Сделайте, в частности, проверку для матриц предельных размеров.
+
+Реализуйте следующие методы:
+Создание случайной матрицы со значениями элементов от 0 до max 
+static int[,] CreateRandomMatrix(int nRows, int nCols, int max)
+
+Распечатка матрицы (для больших матрих выводить только первые 10 строк и столбцов)    
+static void PrintMatrix(int[,] mat)
+
+Сортировка по строкам
+static void SortRows(int[,] mat)
+
+Сортировка по столбцам
+static void SortCols(int[,] mat) {
+
+Проверка факта, что строки остаются в отсортированном состоянии
+static bool CheckMatrixRegularity(int[,] mat) {*/
+
+  static bool CheckMatrixRegularity(int[,] mat) {
+    return true;
+  }
+
 
   static void Demo1() {
     long[] arr1 = { 3, 2, 6, 8, 1, 2, 9, 4, 8, 2 };
@@ -195,9 +287,21 @@ static void SortInside(long[] arr, int iMin, int iMax).
     PrintMatrix(matrix1);
   }
 
+  static void Demo2() {
+    int[] arr1 = { 1, 4, 3, 5, 1, 1, 5, 3, 3, 2, 3 };
+    MaxIdenticalNumbersCount(arr1);
+  }
+  static void Demo3() {
+    int row = 8; int col = 5;
+    int[,] matrix1 = CreateRandomMatrix(row, col, max: 100);
+    PrintMatrix(matrix1);
+    SortRow(matrix1, 2);
+    PrintMatrix(matrix1);
+  }
   static void Main(string[] args) {
-    Demo1();
-
+    //Demo1();
+    //Demo2();
+    Demo3();
 
   }
 }
